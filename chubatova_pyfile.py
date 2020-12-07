71d0f77cdc65e77a83f2a206d1627c2f1965f043
AChubatova <n.chubatova@gmail.com>
2020-12-07 20:41:21 +0100

    req3

--- chubatova_pyfile.py
+++ chubatova_pyfile.py
@@ -1,5 +1,7 @@
 #import par
 #import pmath
+import sys
+import time
 
 
 class Car:
@@ -7,7 +9,7 @@ class Car:
     def __init__(self, speed=0):
         self.speed = speed
         self.odometer = 0
-        self.time = 1
+        self.time = 2
 
     def say_state(self):
         print("I'm going {} kph!".format(self.speed))
